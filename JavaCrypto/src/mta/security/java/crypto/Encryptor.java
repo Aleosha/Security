588b25198289b59466f6ba61e27b81021b7e735f
Aleosha <aleosha@yandex.ru>
2014-12-27 12:51:10 +0200

    Read from flat file

--- JavaCrypto/src/mta/security/java/crypto/Encryptor.java
+++ JavaCrypto/src/mta/security/java/crypto/Encryptor.java
@@ -1,9 +1,32 @@
 package mta.security.java.crypto;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 public class Encryptor {
 
 	public static void main(String[] args) {
+		
+		
+		try {
+			// Read flat file
+			URL messageAsResource = Encryptor.class.getResource("/message.txt");
+			URI uri = messageAsResource.toURI();
+			byte[] content = Files.readAllBytes(Paths.get(uri));
+			System.out.println("The message to encrypt is:" + new String(content));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
