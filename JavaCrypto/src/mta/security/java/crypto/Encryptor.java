e6d19e81e9c9200e106aa404755fbfa89681b99b
Aleosha <aleosha@yandex.ru>
2014-12-27 14:13:07 +0200

    Create methods to write configuration

--- JavaCrypto/src/mta/security/java/crypto/Encryptor.java
+++ JavaCrypto/src/mta/security/java/crypto/Encryptor.java
@@ -1,10 +1,10 @@
 package mta.security.java.crypto;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,9 +12,12 @@ import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.KeyStore;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
+import java.security.SecureRandom;
 import java.security.Signature;
 import java.security.SignatureException;
 
@@ -22,10 +25,18 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 
 public class Encryptor {
+
+	// Provider that we're using
+	private static final String PROVIDER = "SUN";
+	
+	// Path to the file we're encoding
+	private static final String FLAT_FILE_PATH = "/message.txt";
 
 	public static void main(String[] args) {
 		
@@ -34,7 +45,7 @@ public class Encryptor {
 			
 			byte[] content = null;
 			// Read flat file
-			URL messageAsResource = Encryptor.class.getResource("/message.txt");
+			URL messageAsResource = Encryptor.class.getResource(FLAT_FILE_PATH);
 			if (messageAsResource != null) {
 				URI uri = messageAsResource.toURI();
 				content = Files.readAllBytes(Paths.get(uri));
@@ -53,25 +64,64 @@ public class Encryptor {
 			
 			// cipher
 			byte[] cipherText = cipherDigest(digest, privateKey);
+			KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
 			
-			Cipher cipher = Cipher.getInstance("AES");
+			File file = getConfigurationFile();
 			
-			URL configurationFile = Encryptor.class.getResource("/configuration.txt");
-			String path = configurationFile.getPath();
-			File file = new File(path);
-			try (FileOutputStream outputStream = new FileOutputStream(file))
-			{
-				try (CipherOutputStream cipherStream = new CipherOutputStream(outputStream, cipher))
-				{
-					cipherStream.write(signatureBytes);
-				}
-			}
-			
-			
+			writeConfigurationFile(file, signatureBytes);
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * 
+	 * @param file
+	 * @param signatureBytes
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchPaddingException
+	 * @throws InvalidKeyException
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws NoSuchProviderException 
+	 */
+	private static void writeConfigurationFile(File file, byte[] signatureBytes) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, FileNotFoundException, IOException, NoSuchProviderException {
+		// TODO get secret key?
+		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+		 keyGen.init(128);
+		SecretKey secretKey = keyGen.generateKey();
+       
+		Cipher cipher = Cipher.getInstance("AES");
+		
+		SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG", PROVIDER);
+		cipher.init(Cipher.ENCRYPT_MODE, secretKey, secureRandom);
+		
+		try (FileOutputStream outputStream = new FileOutputStream(file))
+		{
+			try (CipherOutputStream cipherStream = new CipherOutputStream(outputStream, cipher))
+			{
+				cipherStream.write(signatureBytes);
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private static File getConfigurationFile() {
+		File file = null;
+		URL configurationFile = Encryptor.class.getResource("/configuration.txt");
+		if (configurationFile == null) {
+			file = new File(Encryptor.class.getResource("/").getPath() + "configuration.txt");
+		}
+		else {
+			String path = configurationFile.getPath();
+			file = new File(path);
+		}
+		
+		return file;
 	}
 
 	/**
@@ -90,7 +140,7 @@ public class Encryptor {
 		cipher.init(Cipher.ENCRYPT_MODE, privateKey);
 		byte[] cipherText = cipher.doFinal(digest);
 		
-		System.out.println("cipher text: " + new String(cipherText));
+		System.out.println("Cipher text: " + new String(cipherText));
 		return cipherText;
 	}
 
